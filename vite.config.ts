6ad5d7671e9912da5de857637086ac64c94ec920
upmediahcm <upconceptstudio@gmail.com>
2026-07-08 04:01:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
+export default defineConfig({
+  //UP-MEDIA-CONTENT
+  base: '/UP-MEDIA-CONTENT/', 
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
